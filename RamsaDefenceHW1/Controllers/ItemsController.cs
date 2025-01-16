45e6b55514b4cf4f9388d0a01766dac1e1a20b45
isology <isoalper-068@hotmail.com>
2025-01-16 20:12:52 +0300

    Proje dosyası ekle.

---
+++ RamsaDefenceHW1/Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RamsaDefenceHW1.Models;
+
+namespace RamsaDefenceHW1.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Items() { Name = "Laptop" };
+            return View(item);
+        }
+    }
+}
---
+++ RamsaDefenceHW1/Models/Items.cs
@@ -0,0 +1,9 @@
+﻿namespace RamsaDefenceHW1.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}

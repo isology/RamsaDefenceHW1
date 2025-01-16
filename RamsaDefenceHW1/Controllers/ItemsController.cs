487a16be2a642b41117177dc0b10218526caba98
isology <isoalper-068@hotmail.com>
2025-01-16 21:50:13 +0300

    Adding 'update' and 'delete' methods

--- RamsaDefenceHW1/Controllers/ItemsController.cs
+++ RamsaDefenceHW1/Controllers/ItemsController.cs
@@ -36,5 +36,44 @@ namespace RamsaDefenceHW1.Controllers
 
             return View(item);
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id );
+            return View(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Price, StockQuantity")] Items item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x =>x.Id == id);
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+            
+        }
     }
 }
